a55d984069c506dd4d5e52ea9b0a1e8c484fc914
Ethorbit <Ethorbit@protonmail.com>
2022-06-17 10:49:41 -0700

    Update isolate-cores.sh

--- isolate-cores.sh
+++ isolate-cores.sh
@@ -7,37 +7,43 @@ ISOLATED_CPU_FILE="/tmp/libvirt-isolated-cpus.txt"
 touch "$ISOLATED_CPU_FILE"
 
 if [ -s "$ISOLATED_CPU_FILE" ]; then 
-        sanitized_core_list=","
+	sanitized_core_list=","
 else
-        sanitized_core_list=""
+	sanitized_core_list=""
 fi
 
-range=$(echo "$ISOLATE_THESE_CORES" | grep -o "\b\([0-9]-[0-9]\)\b")
+range=$(echo "$ISOLATE_THESE_CORES" | grep -o "\b\([0-9]*-[0-9]*\)\b")
 
 if [ $(echo "$range" | wc -c) -gt 1 ]; then # They're adding cores with a range 
-        echo "So you want the range? $range"
-        # Create list based on range by exploding hyphen and using the first (min) and second (max) values
-        # Make sure min is less than max of course
+     range_min=$(echo "$range" | cut -d "-" -f1)
+     range_max=$(echo "$range" | cut -d "-" -f2)  
 
+     for ((i = $range_min; i <= $range_max; i++))
+     {
+	  sanitized_core_list="$sanitized_core_list$i,"	  
+     }
 else # They're adding cores with a comma separated list
-        cores=$(echo "$ISOLATE_THESE_CORES" | grep -o "[0-9]*")
-        
-        for i in $cores; do
-                sanitized_core_list="$sanitized_core_list$i,"
-        done
-
-        sanitized_core_list="${sanitized_core_list::-1}"
+	cores=$(echo "$ISOLATE_THESE_CORES" | grep -o "[0-9]*")
+	
+	for i in $cores; do
+		sanitized_core_list="$sanitized_core_list$i,"
+	done
 fi
 
+if [ -z "$sanitized_core_list" ]; then 
+     exit;
+fi 
+
+sanitized_core_list="${sanitized_core_list::-1}"
 echo "Comma separated list of what you wanted: $sanitized_core_list"
 
-if [ "$IS_ADDING" -ge "1" ]; then       
-        # Make sure if it's a range that it's converted to a comma separated list
-        echo "add to file"              
-        #sed -i "1 s/\$/$ISOLATE_THESE_CORES/" "$ISOLATED_CPU_FILE" 
+if [ "$IS_ADDING" -ge "1" ]; then 	
+	# Make sure if it's a range that it's converted to a comma separated list
+	echo "add to file"		
+	#sed -i "1 s/\$/$sanitized_core_list/" "$ISOLATED_CPU_FILE" 
 else
-        echo "remove from file"
-        #sed -i "1 s/$ISOLATE_THESE_CORES//" "$ISOLATED_CPU_FILE"
+	echo "remove from file"
+	#sed -i "1 s/$sanitized_core_list//" "$ISOLATED_CPU_FILE"
 fi 
 
 isolated_cores=$(cat "$ISOLATED_CPU_FILE")
