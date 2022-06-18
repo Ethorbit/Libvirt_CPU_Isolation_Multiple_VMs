b0cc19fdbfd052bf8319d7d2ff9b7248f120e75a
Ethorbit <Ethorbit@protonmail.com>
2022-06-18 11:07:22 -0700

    Update isolate-cores.sh

--- isolate-cores.sh
+++ isolate-cores.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+IS_VERBOSE="0"
+
 # Create temporary file to manage active core isolations (for all virtual machines)
 ISOLATED_CPU_FILE="/tmp/libvirt-isolated-cpus.txt"
 touch "$ISOLATED_CPU_FILE"
@@ -7,7 +9,9 @@ touch "$ISOLATED_CPU_FILE"
 while [ $# -gt 0 ]; do 
      case $1 in 
 	  -l | --list)
-	       exit; # Implement soon, I guess
+	       # add ability to list a specific virtual machine's cores later
+	       cat "$ISOLATED_CPU_FILE"
+	       exit; 
 	  ;;
 	  -n | --name)
 	       VIRTUAL_MACHINE_NAME="$2"
@@ -21,13 +25,17 @@ while [ $# -gt 0 ]; do
 	  -r | --remove)
 	       IS_ADDING="0"
 	  ;;
+	  -v | --verbose)
+	       IS_VERBOSE="1"
+	  ;;
      esac
      shift
 done
 
 # Add this virtual machine's entry to the file if it doesn't exist:
-if ! cat "$ISOLATED_CPU_FILE" | grep "^$VIRTUAL_MACHINE_NAME"; then 
+if ! cat "$ISOLATED_CPU_FILE" | grep -q "^$VIRTUAL_MACHINE_NAME"; then 
      echo "$VIRTUAL_MACHINE_NAME" >> "$ISOLATED_CPU_FILE"
+     [ "$IS_VERBOSE" -eq 1 ] && echo "Added new entry for $VIRTUAL_MACHINE_NAME" 
 fi
 
 # Process and sanitize --cores 
@@ -37,21 +45,21 @@ if [ $(echo "$range" | wc -c) -gt 1 ]; then # They're adding cores with a range
      range_min=$(echo "$range" | cut -d "-" -f1)
      range_max=$(echo "$range" | cut -d "-" -f2)  
 
-     for ((i = $range_min; i <= $range_max; i++))
-     {
+     [ "$IS_VERBOSE" -eq 1 ] && echo "Adding core range $range_min to $range_max" 
+
+     for ((i = $range_min; i <= $range_max; i++)); do
 	  sanitized_core_list="$sanitized_core_list $i"	  
-     }
+     done
 else # They're adding a separated list
-     cores=$(echo "$ISOLATE_THESE_CORES" | grep -o "[0-9]*")
-	
+     cores=$(echo "$ISOLATE_THESE_CORES" | grep -o "[0-9]*") 
+
      for i in $cores; do
 	  sanitized_core_list="$sanitized_core_list $i"
      done
 fi
 
-if [ -z "$sanitized_core_list" ]; then 
-     exit;
-fi 
+[ "$IS_VERBOSE" -eq 1 ] && echo "Processed core list: $sanitized_core_list" 
+[ -z "$sanitized_core_list" ] && exit; 
 
 if [ "$IS_ADDING" -ge 1 ]; then 			
      sed -i "/^$VIRTUAL_MACHINE_NAME/c $VIRTUAL_MACHINE_NAME $sanitized_core_list" "$ISOLATED_CPU_FILE" 
@@ -63,21 +71,16 @@ fi
 all_isolated_cores=$(cat "$ISOLATED_CPU_FILE")
 allowed_cores=""
 
-for i in $(lscpu -e | awk '{print $1}'); do  
-     if ! echo "$all_isolated_cores" | grep "\s$i\s"; then 
+for ((i = 0; i <= $(nproc --all); i++)); do  
+     if ! echo "$all_isolated_cores" | grep -q "\s$i\s"; then 
 	  allowed_cores="$allowed_cores$i,"
      fi 
 done
 
-if [ -z "$allowed_cores" ]; then 
-     exit;
-fi
-
+[ -z "$allowed_cores" ] && exit;
 allowed_cores="${allowed_cores::-1}"
 
-echo "Finally we will only allow these cores on the host: $allowed_cores"
-
-# Isolate the cores from the variable read above
-#systemctl set-property --runtime -- system.slice AllowedCPUs=4,5,6,7
-#systemctl set-property --runtime -- user.slice AllowedCPUs=4,5,6,7
-#systemctl set-property --runtime -- init.scope AllowedCPUs=4,5,6,7
+[ "$IS_VERBOSE" -eq 1 ] && echo "Cores allowed on host: $allowed_cores" 
+#systemctl set-property --runtime -- system.slice AllowedCPUs=$allowed_cores
+#systemctl set-property --runtime -- user.slice AllowedCPUs=$allowed_cores
+#systemctl set-property --runtime -- init.scope AllowedCPUs=$allowed_cores
